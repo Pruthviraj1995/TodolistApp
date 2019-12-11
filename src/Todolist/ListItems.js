b37e13cba486f163725f16f4ec7b7e0b0195d0a7
Gowtham Raj <gowthamraj.r15@gmail.com>
2019-12-12 01:30:35 +0530

    scroll issue fix

--- src/Todolist/ListItems.js
+++ src/Todolist/ListItems.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import List from './List';
-
+import './style.scss'
 const ListItems = ({data,handleToggle, deleteItems}) =>{
     const completed=data.filter(el=>el.completed);
     const ongoing=data.filter(el=>!el.completed);
     const displaydata=[...ongoing,...completed];
 
     return( 
-        <div>
+        <div className="listbody">
         {displaydata.map((todo) => (
         <div onClick={()=>handleToggle(todo.id)}>
             <List key={todo} {...todo} deleteItems={deleteItems} uid={todo.id}/>
